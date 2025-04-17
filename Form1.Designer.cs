8a0f259f0a9b05f210817d6ddd616d5ccbed933f
Mihaly24 <fairuzk20@gmail.com>
2025-04-17 12:06:54 +0700

    Menambahkan deisgn form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Nama = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.NoTelp = new System.Windows.Forms.Label();
+            this.Alamat = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNoTelp = new System.Windows.Forms.TextBox();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(68, 21);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(44, 16);
+            this.Nama.TabIndex = 0;
+            this.Nama.Text = "Nama";
+            this.Nama.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(68, 60);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(41, 16);
+            this.Email.TabIndex = 1;
+            this.Email.Text = "Email";
+            // 
+            // NoTelp
+            // 
+            this.NoTelp.AutoSize = true;
+            this.NoTelp.Location = new System.Drawing.Point(68, 101);
+            this.NoTelp.Name = "NoTelp";
+            this.NoTelp.Size = new System.Drawing.Size(79, 16);
+            this.NoTelp.TabIndex = 2;
+            this.NoTelp.Text = "No Telepon";
+            // 
+            // Alamat
+            // 
+            this.Alamat.AutoSize = true;
+            this.Alamat.Location = new System.Drawing.Point(68, 144);
+            this.Alamat.Name = "Alamat";
+            this.Alamat.Size = new System.Drawing.Size(49, 16);
+            this.Alamat.TabIndex = 3;
+            this.Alamat.Text = "Alamat";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(240, 21);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(454, 22);
+            this.txtNama.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(240, 60);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(454, 22);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtNoTelp
+            // 
+            this.txtNoTelp.Location = new System.Drawing.Point(240, 101);
+            this.txtNoTelp.Name = "txtNoTelp";
+            this.txtNoTelp.Size = new System.Drawing.Size(454, 22);
+            this.txtNoTelp.TabIndex = 6;
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(240, 144);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(454, 22);
+            this.txtAlamat.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 238);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 200);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(71, 200);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(86, 23);
+            this.btnTambah.TabIndex = 9;
+            this.btnTambah.Text = "Add";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(262, 200);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(78, 23);
+            this.btnUpdate.TabIndex = 10;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(432, 200);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(87, 23);
+            this.btnHapus.TabIndex = 11;
+            this.btnHapus.Text = "Delete";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(614, 198);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 26);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.txtNoTelp);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.Alamat);
+            this.Controls.Add(this.NoTelp);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Nama);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label NoTelp;
+        private System.Windows.Forms.Label Alamat;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNoTelp;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -21,5 +21,10 @@ namespace Project
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
